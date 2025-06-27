2d10e032b3df8c8575ea02f14b38f68384660ad2
Bao <nhanhem2ui@gmail.com>
2025-06-27 18:48:52 +0700

    [ADD]: Homepage, auth view, user profile

---
+++ ChickenFlickFilmApplication/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChickenFlickFilmApplication.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
