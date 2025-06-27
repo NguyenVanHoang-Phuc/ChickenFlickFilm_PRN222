23ba9d25afa60e5a84eb747fef6369dc3a46f51d
NguyenVanHoangPhuc <hoangphucnguyenvan1@gmail.com>
2025-06-27 16:52:46 +0700

    Add project files.

---
+++ ChickenFlickFilmApplication/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChickenFlickFilmApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Auditoriums()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Movies()
+        {
+            return View();
+        }
+        public IActionResult Showtimes()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Theaters()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChickenFlickFilmApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChickenFlickFilmApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
