14337b051a57e450e9d77be95909ebd56ce93965
qc01696@gmail.com <0396866849Qcu@1609>
2025-07-08 16:09:01 +0700

    scaffold database

---
+++ BusinessObjects/Models/Theater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects.Models;
+
+public partial class Theater
+{
+    public int TheaterId { get; set; }
+
+    public string TheaterName { get; set; } = null!;
+
+    public string? MapUrl { get; set; }
+
+    public string Location { get; set; } = null!;
+
+    public int TotalTheaters { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<Auditorium> Auditoria { get; set; } = new List<Auditorium>();
+}
