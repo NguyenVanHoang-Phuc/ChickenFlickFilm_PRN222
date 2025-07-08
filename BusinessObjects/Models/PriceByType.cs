9665cbc416ab0cc49114b378470a0ebf4336ccf9
NguyenVanHoangPhuc <hoangphucnguyenvan1@gmail.com>
2025-07-08 15:01:48 +0700

    Scafford models, configure program, dbcontext

---
+++ BusinessObjects/Models/PriceByType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects.Models;
+
+public partial class PriceByType
+{
+    public string Type { get; set; } = null!;
+
+    public decimal? Price { get; set; }
+
+    public virtual ICollection<Auditorium> Auditoria { get; set; } = new List<Auditorium>();
+
+    public virtual ICollection<Seat> Seats { get; set; } = new List<Seat>();
+}
---
+++ BusinessObjects/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? FullName { get; set; }
+
+    public string Avatar { get; set; } = null!;
+
+    public string? Role { get; set; }
+
+    public bool Gender { get; set; }
+
+    public DateTime Birthday { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+}
