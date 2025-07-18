951b924f79ef4077542721c597f5487e936dd62a
Pham Thi Thuy Linh <thuylinhcua2024@gmail.com>
2025-07-18 11:38:00 +0700

    Add classes, libraries and views in makePayment function

---
+++ ChickenFlickFilmApplication/Controllers/CheckoutController.cs
@@ -0,0 +1,31 @@
+﻿using ChickenFlickFilmApplication.Services.VnPay;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChickenFlickFilmApplication.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly IVnPayService _vnPayService;
+        private readonly IEmailSender _emailSender;
+        public CheckoutController(IVnPayService vnPayService, IEmailSender emailSender)
+        {
+            _vnPayService = vnPayService;
+            _emailSender = emailSender;
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult PaymentCallbackVnpay()
+        {
+            var response = _vnPayService.PaymentExecute(Request.Query);
+
+            return Json(response);
+        }
+
+    }
+}
---
+++ ChickenFlickFilmApplication/Controllers/PaymentController.cs
@@ -0,0 +1,29 @@
+﻿using ChickenFlickFilmApplication.PaymentGatewayIntegration.VnPay;
+using ChickenFlickFilmApplication.Services.VnPay;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChickenFlickFilmApplication.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IVnPayService _vnPayService;
+        public IActionResult BookingDetails()
+        {
+            return View();
+        }
+        public PaymentController(IVnPayService vnPayService)
+        {
+
+            _vnPayService = vnPayService;
+        }
+
+        public IActionResult CreatePaymentUrlVnpay(PaymentInformationModel model)
+        {
+            var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
+
+            return Redirect(url);
+        }
+       
+
+    }
+}
